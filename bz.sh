4dbcb956ebcdcd2b4ca65cda9b44c1627cb81daf
chenwumm <158730941+chenwumm@users.noreply.github.com>
2024-06-15 20:40:15 +0800

    Update bz.sh

--- bz.sh
+++ bz.sh
@@ -1,8 +1,6 @@
 echo "晨雾扒站服务"
 read -p "请输入你要扒的网站(记得加上https://和后面的/):" ws
 echo "正在扒取,请耐心等待..."
-mkdir $ws
-touch $ws index.html
 html=`curl -s $ws`
-echo $html > $wsindex.html
+echo $html > $ws/index.html
 
