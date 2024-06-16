27f0001d461ac1603bc17a66df7bd43f0ce404a2
chenwumm <158730941+chenwumm@users.noreply.github.com>
2024-06-16 09:22:08 +0800

    Update bz.sh

--- bz.sh
+++ bz.sh
@@ -1,10 +1,10 @@
 echo "晨雾扒站服务"
 read -p "请输入你要扒的网站(记得加上https://和后面的/):" ws
 echo "正在扒取,请耐心等待..."
-mkdir 晨雾扒站服务
-touch 晨雾扒站服务/index.html
-html=`curl -s $ws`
+if [ -f ~/晨雾扒站服务 ]; then html=`curl -s $ws`
 echo $html > 晨雾扒站服务/index.html
 echo "扒站成功,保存在 ~/晨雾扒站服务/index.html"
 echo "你可以输入cat 晨雾扒站服务/index.html来查看这个文件"
 echo "晨雾官网:https://chenwumm.github.io/ 欢迎访问!"
+else mkdir 晨雾扒站服务;touch 晨雾扒站服务/index.html
+fi
